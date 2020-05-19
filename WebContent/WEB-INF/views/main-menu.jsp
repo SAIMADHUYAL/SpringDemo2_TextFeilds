116959e7c5d2508cf4d0184f1460b3577e5cbd2a
Saimadhu Yelakkayala <saimadhu.yal@gmail.com>
2020-05-19 14:01:27 -0500

    Initial Commit

---
+++ WebContent/WEB-INF/views/main-menu.jsp
@@ -0,0 +1,11 @@
+  
+<!DOCTYPE html>
+<html>
+
+	<body>
+
+		<h2>Spring MVC Demo Page</h2>
+		<hr>
+		<a href="showForm">Student Form</a>
+	</body>
+</html>
---
+++ src/com/saimadhu/textfeilds/HomeController.java
@@ -0,0 +1,14 @@
+package com.saimadhu.textfeilds;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String homeMapping() {
+		return "main-menu";
+	}
+
+}
